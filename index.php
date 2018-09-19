bdc7a866c2ce3de57fcd1e918ed2ba3a3c620663
Hampus Björn <31540371+HampusBjorn@users.noreply.github.com>
2018-09-19 11:10:24 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
 	<title>Soon there!</title>
 	<style type="text/css">
 		main{
 			width: 80%;
 			margin: 0px auto;
+			font-family: 'Merriweather', serif;
 		}
 	</style>
 </head>
--- login.php
+++ login.php
@@ -1,9 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>You are there</title>
 </head>
 <body>
+<?php  
+session_start();
 
+if (isset($_POST['submit'])) {
+
+	$loginCredentials = [
+		"username" => "Hampus.bjorn",
+		"password" => "secure"
+	];
+
+	if (isset($_POST['username']) && isset($_POST['password'])) {
+		$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+		$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+		$hash = $password;
+
+			if ($username == $loginCredentials['username'] && $password == password_verify($loginCredentials['password'], $hash)) {
+				echo "<h1>Välkommen till alla hemliga saker</h1>";
+			}
+
+			elseif ($username != $loginCredentials['username'] || $password != password_verify($loginCredentials['password'], $hash)) {
+				echo "<h1>Användarnamn eller lösenord är fel</h1>";
+			}
+
+			else {
+				echo "<h1>FEL!</h1>";
+			}
+	}
+
+}
+
+else {
+	echo "<h1>Dumma jävel</h1>";
+}
+
+?>
 </body>
 </html>
